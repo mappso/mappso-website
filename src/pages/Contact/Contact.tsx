a6b4b4662a3ffbfe08471e1aae511b2313bd981d
tomilodk <tomilobas@gmail.com>
2023-07-11 06:35:55 +0200

    Contact me at (For apple)

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-
+import './Contact.scss';
 interface Props {
 
 }
 
 const Contact: React.FC<Props> = (props) => {
    return (
-      <div>
-         <h1>The site is under development</h1>
+      <div className="contact">
+         <h1>Contact me at</h1>
          <br/>
          <p>contact@mappso.com</p>
       </div>
