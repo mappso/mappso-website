45ef460284783b4edfe7c0c358d644555d65ced2
tomilodk <tomilobas@gmail.com>
2024-03-25 15:54:03 +0100

    Removed unused action

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
 import "./App.scss";
-import NavMenu from "./components/NavMenu";
 import Resume from "./pages/Resume/Resume";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Contact from "./pages/Contact/Contact";
@@ -8,19 +6,16 @@ import Layout from "./components/Layout";
 import NotFound from "./pages/NotFound/NotFound";
 
 function App() {
-    const action = () => {
-        console.log("Clicked");
-    }
     return (
         <div className="App">
             <header className="App-header"></header>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" action={action} element={<Layout />}>
-                        <Route index action={action} element={<Resume />} />
-                        <Route path="contact" action={action} element={<Contact />} />
-                        <Route path="test" action={action} element={<Contact />} />
-                        <Route path="*" action={action} element={<NotFound />} />
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Resume />} />
+                        <Route path="contact" element={<Contact />} />
+                        <Route path="test" element={<Contact />} />
+                        <Route path="*" element={<NotFound />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
