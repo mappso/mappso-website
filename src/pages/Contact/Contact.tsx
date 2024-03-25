93ff200618577286db5e5c42ad19de11f1f1604d
tomilodk <tomilobas@gmail.com>
2024-03-25 15:54:10 +0100

    Added contact

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import "./Contact.scss";
 
 interface Props {
 
 }
 
 const Contact: React.FC<Props> = (props) => {
+   const code = `
+const mail = "contact@mappso.com";
+const cvr = "42149705";
+`;
    return (
-      <div>
-         <h1>Contact</h1>
-         <p>Hej<br/><br/><br/><br/><br/><br/>Hej2</p>
+      <div className="contact">
+         <CodeEditor className="code" value={code} language="typescript" readOnly></CodeEditor>
       </div>
    );
 }
