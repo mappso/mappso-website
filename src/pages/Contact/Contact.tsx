f6126a1018da6d88a189a2adef8ae18520dd6bd3
Milo <tomilobas@gmail.com>
2024-03-28 15:07:44 +0100

    Only initialize once when using React.strict

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,78 +1,59 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import CodeEditor from "@uiw/react-textarea-code-editor";
 import copy from "copy-to-clipboard";
 import "./Contact.scss";
 
-interface Props {}
+interface Props { }
 
 const Contact: React.FC<Props> = (props) => {
-    const clipBoardElements = ["contact@mappso.com", "42149705"];
+   const clipBoardElements = ["contact@mappso.com", "42149705"];
+   const initializedRef = useRef(false); // Ref to track initialization
 
-    const addToClipboardAction = (span: Element) => {
-        let clipBoardElement = "";
+   const addToClipboardAction = (span: Element) => {
+      let clipBoardElement = "";
 
-        for (const possibleElement of clipBoardElements) {
-            if (span.innerHTML.includes(possibleElement)) {
-                clipBoardElement = possibleElement;
-            }
-        }
+      for (const possibleElement of clipBoardElements) {
+         if (span.innerHTML.includes(possibleElement)) {
+            clipBoardElement = possibleElement;
+         }
+      }
 
-        const button = document.createElement("button");
+      const button = document.createElement("button");
+      button.classList.add("copy");
+      button.addEventListener("click", () => {
+         copy(clipBoardElement);
+      });
 
-        button.classList.add("no-style");
+      const parent = span.parentNode;
+      parent?.replaceChild(button, span);
+      button.appendChild(span);
+   };
 
-        button.addEventListener("click", () => {
-            copy(clipBoardElement);
-        });
+   const code = `const mail = "contact@mappso.com";\nconst cvr = "42149705";`;
 
-        const parent = span.parentNode;
-        parent?.replaceChild(button, span);
+   useEffect(() => {
+      if (!initializedRef.current) {
+         const spanNodes = document.querySelectorAll("span.token.string");
 
-        button.appendChild(span);
-    };
-
-    const code = `const mail = "contact@mappso.com";
-const cvr = "42149705";`;
-
-    useEffect(() => {
-        const spanNodes = document.querySelectorAll("span.token.string");
-
-        for (const span of spanNodes) {
+         for (const span of spanNodes) {
             if (clipBoardElements.some((e) => span.innerHTML.includes(e))) {
-                addToClipboardAction(span);
+               addToClipboardAction(span);
             }
-        }
-    }, []);
+         }
+         initializedRef.current = true;
+      }
+   }, [clipBoardElements]);
 
-    return (
-        <div className="contact">
-            <CodeEditor
-                className="code"
-                value={code}
-                language="typescript"
-                // rehypePlugins={[
-                //    [rehypePrism as any, { ignoreMissing: true }],
-                //    [
-                //      rehypeRewrite as any,
-                //      {
-                //        rewrite: (node: any, index: number, parent: any) => {
-                //          if (node.properties?.className?.includes("code-line")) {
-                //            if (index === 0 && node.properties?.className) {
-                //              node.properties.className.push("demo01");
-                //              // console.log("~~~", index, node.properties?.className);
-                //            }
-                //          }
-                //          if (node.type === "text" && node.value === "return" && parent.children.length === 1) {
-                //            parent.properties.className.push("demo123");
-                //          }
-                //        }
-                //      }
-                //    ]
-                //  ]}
-                readOnly
-            ></CodeEditor>
-        </div>
-    );
+   return (
+      <div className="contact">
+         <CodeEditor
+            className="code"
+            value={code}
+            language="typescript"
+            readOnly
+         ></CodeEditor>
+      </div>
+   );
 };
 
 export default Contact;
